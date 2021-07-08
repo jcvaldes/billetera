bdc4d4061376703c23fea5ee761ad6e80442296b
Juan Carlos Valdés <idevkingos@gmail.com>
2021-07-08 13:40:41 -0300

    feat: agrego theme light y dark

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  background-color: ${props => props.theme.colors.primary};
+  color: ${props => props.theme.colors.white};
   grid-area: CT;
-  background-color: #1b1f38;
 `;
