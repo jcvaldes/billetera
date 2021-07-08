5da7ce0539f36c5323103a0237c3b899a24d10e5
Juan Carlos Valdés <idevkingos@gmail.com>
2021-07-08 15:42:50 -0300

    feat: agrego theme light y dark

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,9 +1,21 @@
-import React from "react";
-import { Container } from "./styles";
+import React, { useMemo } from 'react';
+import { Container, Profile, Welcome, UserName } from './styles';
+import emojis from '../../utils/emojis';
+
 const MainHeader: React.FC = () => {
+  // useMemo: hook q se dispara cuando el valor cambia
+  const emoji = useMemo(() => {
+    const indice = Math.floor(Math.random() * emojis.length);
+    // console.log(indice);
+    return emojis[indice];
+  }, []);
   return (
     <Container>
-      <h1>MainHeader</h1>
+      <h1>Toogle</h1>
+      <Profile>
+        <Welcome>Hola, {emoji}</Welcome>
+        <UserName>Juan Carlos</UserName>
+      </Profile>
     </Container>
   );
 };
--- src/components/MainHeader/styles.ts
+++ src/components/MainHeader/styles.ts
@@ -1,7 +1,18 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
-  background-color: ${props => props.theme.colors.secondary};
-  color: ${props => props.theme.colors.white};
+  align-items: center;
+  background-color: ${(props) => props.theme.colors.secondary};
+  border-bottom: 1px solid ${(props) => props.theme.colors.gray};
+  display: flex;
   grid-area: MH;
+  justify-content: space-between;
+  padding: 0 10px;
 `;
+
+export const Profile = styled.div`
+  color: ${(props) => props.theme.colors.white};
+`;
+
+export const Welcome = styled.h3``;
+export const UserName = styled.span``;
