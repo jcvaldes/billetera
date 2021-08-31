e8e57da1b1f1c2acaf7cc89befc4482842cf9084
Juan Carlos Valdés <idevkingos@gmail.com>
2021-08-31 13:53:00 -0300

    feat: filtros

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -9,13 +9,13 @@ interface IRouteParams {
     params: {
       // type tengo que especificar el parametro que recibi en la ruta en el archivo de rouutes
       type: string;
-    }
-  }
+    };
+  };
 }
 // match está disponible gracias al react-router-dom
 const List: React.FC<IRouteParams> = ({ match }) => {
   // useMemo: hook para memorizar un valor
-  const {type} = match.params;
+  const { type } = match.params;
   const title = useMemo(() => {
     return type === 'entry-balance' ? 'Entradas' : 'Salidas';
   }, [type]); // en el array puedo especificar una variable para  que actulice cuando un el valor de dicha variable cambia
@@ -24,20 +24,21 @@ const List: React.FC<IRouteParams> = ({ match }) => {
     return type === 'entry-balance' ? '#F7931B' : '#F44C4E';
   }, [type]);
 
-  const options = [
-    {
-      value: 'Juan',
-      label: 'Juan',
-    },
-    {
-      value: 'Joaquin',
-      label: 'Joaquin',
-    },
+  const months = [
+    { value: 7, label: 'Julio' },
+    { value: 8, label: 'Agosto' },
+    { value: 9, label: 'Septiembre' },
+  ];
+  const years = [
+    { value: 2020, label: 2020 },
+    { value: 2019, label: 2019 },
+    { value: 2018, label: 2018 },
   ];
   return (
     <Container>
       <ContentHeader title={title} lineColor={lineColor}>
-        <SelectInput options={options} />
+        <SelectInput options={months} />
+        <SelectInput options={years} />
       </ContentHeader>
       <Filters>
         <button type="button" className="tag-filter tag-filter-recurrent">
@@ -54,67 +55,67 @@ const List: React.FC<IRouteParams> = ({ match }) => {
           subtitle="27/07/2020"
           amount="R$ 130,00"
         />
-             <HistoryFinanceCard
+        <HistoryFinanceCard
           tagColor="#E44C4E"
           title="Cuenta de luz"
           subtitle="27/07/2020"
           amount="R$ 130,00"
         />
-             <HistoryFinanceCard
+        <HistoryFinanceCard
           tagColor="#E44C4E"
           title="Cuenta de luz"
           subtitle="27/07/2020"
           amount="R$ 130,00"
         />
-             <HistoryFinanceCard
+        <HistoryFinanceCard
           tagColor="#E44C4E"
           title="Cuenta de luz"
           subtitle="27/07/2020"
           amount="R$ 130,00"
         />
-             <HistoryFinanceCard
+        <HistoryFinanceCard
           tagColor="#E44C4E"
           title="Cuenta de luz"
           subtitle="27/07/2020"
           amount="R$ 130,00"
         />
-             <HistoryFinanceCard
+        <HistoryFinanceCard
           tagColor="#E44C4E"
           title="Cuenta de luz"
           subtitle="27/07/2020"
           amount="R$ 130,00"
         />
-             <HistoryFinanceCard
+        <HistoryFinanceCard
           tagColor="#E44C4E"
           title="Cuenta de luz"
           subtitle="27/07/2020"
           amount="R$ 130,00"
         />
-             <HistoryFinanceCard
+        <HistoryFinanceCard
           tagColor="#E44C4E"
           title="Cuenta de luz"
           subtitle="27/07/2020"
           amount="R$ 130,00"
         />
-             <HistoryFinanceCard
+        <HistoryFinanceCard
           tagColor="#E44C4E"
           title="Cuenta de luz"
           subtitle="27/07/2020"
           amount="R$ 130,00"
         />
-             <HistoryFinanceCard
+        <HistoryFinanceCard
           tagColor="#E44C4E"
           title="Cuenta de luz"
           subtitle="27/07/2020"
           amount="R$ 130,00"
         />
-             <HistoryFinanceCard
+        <HistoryFinanceCard
           tagColor="#E44C4E"
           title="Cuenta de luz"
           subtitle="27/07/2020"
           amount="R$ 130,00"
         />
-             <HistoryFinanceCard
+        <HistoryFinanceCard
           tagColor="#E44C4E"
           title="Cuenta de luz"
           subtitle="27/07/2020"
